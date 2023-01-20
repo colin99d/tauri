7fa40c80b1886b2deab82f6d9027a866743811b7
colin99d <colin99delahunty@gmail.com>
2023-01-20 10:56:05 -0500

    So close

--- examples/sidecar/src-tauri/src/main.rs
+++ examples/sidecar/src-tauri/src/main.rs
@@ -7,38 +7,94 @@
   windows_subsystem = "windows"
 )]
 
-use tauri::{
-  api::process::{Command, CommandEvent},
-  Manager,
-};
+use std::path::PathBuf;
+use tauri::api::process::{Command, CommandEvent};
+use tauri::Manager;
+use tauri::{async_runtime, generate_context, generate_handler, Builder};
 
 fn main() {
   tauri::Builder::default()
     .setup(|app| {
-      let window = app.get_window("main").unwrap();
-      tauri::async_runtime::spawn(async move {
-        let (mut rx, mut child) = Command::new_sidecar("app")
-          .expect("failed to setup `app` sidecar")
-          .spawn()
-          .expect("Failed to spawn packaged node");
+      let target_port = get_available_port().map_or_else(
+        || {
+          println!("All ports from 14733 to 16789 are taken");
+          0
+        },
+        |port| port,
+      );
+      /*
+        let window = app.get_window("main").unwrap();
+        tauri::async_runtime::spawn(async move {
+          let (mut rx, _) = Command::new_sidecar("app")
+            .unwrap()
+            .spawn()
+            .unwrap();
 
-        let mut i = 0;
-        while let Some(event) = rx.recv().await {
-          if let CommandEvent::Stdout(line) = event {
-            window
-              .emit("message", Some(format!("'{}'", line)))
-              .expect("failed to emit event");
-            i += 1;
-            if i == 4 {
-              child.write("message from Rust\n".as_bytes()).unwrap();
-              i = 0;
+          println!("PART ONE PASSED");
+          while let Some(event) = rx.recv().await {
+            if let CommandEvent::Stdout(line) = event {
+              window
+                .emit("message", Some(format!("'{}'", line)))
+                .unwrap();
             }
           }
+        });
+      */
+      let resource_path = match app
+        .path_resolver()
+        .resolve_resource("binaries/app-aarch64-apple-darwin")
+      {
+        None => {
+          println!("Could not get the file path for the OpenBB binary");
+          // TODO: not sure the best way to handle this
+          return Ok(());
+        }
+        Some(path) => path,
+      };
+
+      async_runtime::spawn(async move {
+        if let Err(error) = launch_sdk(resource_path, &target_port.to_string()).await {
+          println!("Error launching sdk: {}", error);
         }
       });
-
       Ok(())
     })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+pub async fn launch_sdk(resource_path: PathBuf, target_port: &str) -> Result<(), String> {
+  let path_str = match resource_path.as_path().to_str() {
+    None => return Err("Could not convert the given path to a string".to_string()),
+    Some(path) => path,
+  };
+  println!("{}", path_str);
+  let (mut rx, _child) = Command::new(
+    "/Users/colindelahunty/tauri/examples/sidecar/src-tauri/binaries/app-aarch64-apple-darwin",
+  )
+  // .args(["--rest", "--port", target_port])
+  .spawn()
+  .unwrap();
+
+  while let Some(event) = rx.recv().await {
+    println!("EVENT: {:?}", event);
+    if let CommandEvent::Stdout(line) = event {
+      println!("{}", line);
+    }
+  }
+  Ok(())
+}
+
+use std::net::TcpListener;
+
+pub fn get_available_port() -> Option<u16> {
+  (14733..16789).find(|port| port_is_available(*port))
+}
+
+/// Checks whether a port is available given it is not being used on the host localhost, 0.0.0.0,
+/// or 127.0.0.1. This could have an issue if the user has the port active on a different host
+fn port_is_available(port: u16) -> bool {
+  TcpListener::bind(("localhost", port)).is_ok()
+    && TcpListener::bind(("0.0.0.0", port)).is_ok()
+    && TcpListener::bind(("127.0.0.1", port)).is_ok()
 }
--- examples/sidecar/src/index.js
+++ examples/sidecar/src/index.js
@@ -1,9 +1,222 @@
-const { write, onMessage } = require('./communication')
+const fs = require("fs");
+const process = require("process");
 
-onMessage((line) => {
-  write(`read ${line}`)
-})
+const express = require("express");
+const ws = require("express-ws");
+const pty = require("node-pty");
 
-setInterval(() => {
-  write(`[${new Date().toLocaleTimeString()}] new message`)
-}, 500)
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>OpenBB Terminal</title>
+    <style>
+      .xterm {
+          font-feature-settings: "liga" 0;
+          position: relative;
+          user-select: none;
+          -ms-user-select: none;
+          -webkit-user-select: none;
+      }
+
+      .xterm.focus,
+      .xterm:focus {
+          outline: none;
+      }
+
+      .xterm .xterm-helpers {
+          position: absolute;
+          top: 0;
+          /**
+           * The z-index of the helpers must be higher than the canvases in order for
+           * IMEs to appear on top.
+           */
+          z-index: 5;
+      }
+
+      .xterm .xterm-helper-textarea {
+          padding: 0;
+          border: 0;
+          margin: 0;
+          /* Move textarea out of the screen to the far left, so that the cursor is not visible */
+          position: absolute;
+          opacity: 0;
+          left: -9999em;
+          top: 0;
+          width: 0;
+          height: 0;
+          z-index: -5;
+          /** Prevent wrapping so the IME appears against the textarea at the correct position */
+          white-space: nowrap;
+          overflow: hidden;
+          resize: none;
+      }
+
+      .xterm .composition-view {
+          /* TODO: Composition position got messed up somewhere */
+          background: #000;
+          color: #FFF;
+          display: none;
+          position: absolute;
+          white-space: nowrap;
+          z-index: 1;
+      }
+
+      .xterm .composition-view.active {
+          display: block;
+      }
+
+      .xterm .xterm-viewport {
+          /* On OS X this is required in order for the scroll bar to appear fully opaque */
+          background-color: #000;
+          overflow-y: scroll;
+          cursor: default;
+          position: absolute;
+          right: 0;
+          left: 0;
+          top: 0;
+          bottom: 0;
+      }
+
+      .xterm .xterm-screen {
+          position: relative;
+      }
+
+      .xterm .xterm-screen canvas {
+          position: absolute;
+          left: 0;
+          top: 0;
+      }
+
+      .xterm .xterm-scroll-area {
+          visibility: hidden;
+      }
+
+      .xterm-char-measure-element {
+          display: inline-block;
+          visibility: hidden;
+          position: absolute;
+          top: 0;
+          left: -9999em;
+          line-height: normal;
+      }
+
+      .xterm {
+          cursor: text;
+      }
+
+      .xterm.enable-mouse-events {
+          /* When mouse events are enabled (eg. tmux), revert to the standard pointer cursor */
+          cursor: default;
+      }
+
+      .xterm.xterm-cursor-pointer {
+          cursor: pointer;
+      }
+
+      .xterm.column-select.focus {
+          /* Column selection mode */
+          cursor: crosshair;
+      }
+
+      .xterm .xterm-accessibility,
+      .xterm .xterm-message {
+          position: absolute;
+          left: 0;
+          top: 0;
+          bottom: 0;
+          right: 0;
+          z-index: 10;
+          color: transparent;
+      }
+
+      .xterm .live-region {
+          position: absolute;
+          left: -9999px;
+          width: 1px;
+          height: 1px;
+          overflow: hidden;
+      }
+
+      .xterm-dim {
+          opacity: 0.5;
+      }
+
+      .xterm-underline {
+          text-decoration: underline;
+      }
+    </style>
+    <style>
+      #terminal {
+        height: 100vh;
+        width: 100vw;
+        position:absolute;
+        top:0px;
+        right:0px;
+        background-color: black;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="terminal"></div>
+    <script src="https://unpkg.com/xterm@4.11.0/lib/xterm.js"></script>
+    <script src="https://unpkg.com/xterm-addon-attach@0.6.0/lib/xterm-addon-attach.js"></script>
+    <script src="https://unpkg.com/xterm-addon-fit@0.5.0/lib/xterm-addon-fit.js"></script>
+    <script src="https://unpkg.com/xterm-addon-web-links@0.6.0/lib/xterm-addon-web-links.js"></script>
+    <script>
+      const term = new Terminal();
+      const socket = new WebSocket("ws://localhost:80/ws");
+      // const weblinks = new WebLinksAddon();
+
+      const websocketAddon = new AttachAddon.AttachAddon(socket);
+      const resizeAddon = new FitAddon.FitAddon();
+
+      term.loadAddon(websocketAddon);
+      term.loadAddon(resizeAddon);
+      // terminal.loadAddon(weblinks);
+
+      term.open(document.getElementById("terminal"));
+
+      resizeAddon.fit();
+      window.addEventListener("resize", () => resizeAddon.fit());
+    </script>
+  </body>
+</html>
+
+`
+
+const app = express();
+
+const expressWs = ws(app);
+
+console.log("Listening on port 80");
+
+app.get("/", (req, res) => {
+  console.log("GET /");
+  res.setHeader("Content-Type", "text/html");
+  res.send(html);
+});
+app.ws("/ws", (ws) => {
+  console.log("WS /ws");
+  const term = pty.spawn("zsh", [], { name: "xterm-color", cols: 170, rows: 60 });
+  // run command in shell
+  term.write("conda activate obb && python /Users/colindelahunty/OpenBBTerminal/terminal.py\n")
+  setTimeout(() => term.kill(), 3600 * 1000); // session timeout
+  term.on("data", (data) => {
+    try {
+      console.log("data: ", data);
+      ws.send(data);
+    } catch (err) {}
+  });
+  ws.on("message", (data) => {
+    console.log("message: ", data);
+    term.write(data)
+  });
+});
+
+// Prevent malformed packets from crashing server.
+expressWs.getWss().on("connection", (ws) => ws.on("error", console.error));
+
+app.listen(80, "0.0.0.0");
+
